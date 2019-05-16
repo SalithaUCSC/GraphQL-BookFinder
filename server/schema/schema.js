c863a51d2795a9a2221080b01e02ee1b41b7de3e
Salitha Chathuranga <salithachathuranga94@gmail.com>
2019-05-16 13:11:00 +0530

    add other object types with relations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,21 +1,59 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const { 
+    GraphQLObjectType, 
+    GraphQLString, 
+    GraphQLSchema,
+    GraphQLID,
+    GraphQLInt,
+    GraphQLList
+} = graphql;
 const lodash = require('lodash');
 
 //dummy data
 var books = [
-    { title: 'Book 1', category: 'Romance', id: '1' },
-    { title: 'Book 2', category: 'Science', id: '2' },
-    { title: 'Book 3', category: 'Fantasy', id: '3' }
+    { title: 'Book 1', category: 'Romance', id: '1', authorId: '1' },
+    { title: 'Book 2', category: 'Science', id: '2', authorId: '2' },
+    { title: 'Book 3', category: 'Fantasy', id: '3', authorId: '3' },
+    { title: 'Book 4', category: 'Maths', id: '4', authorId: '2' },
+    { title: 'Book 5', category: 'Science', id: '5', authorId: '1' },
+    { title: 'Book 6', category: 'Fantasy', id: '6', authorId: '2' }
+]
+
+var authors = [
+    { name: 'Peter Johnson', age: 35, id: '1' },
+    { name: 'Andrew Simpson', age: 43, id: '2' },
+    { name: 'Michael Peterson', age: 29, id: '3' }
 ]
 
 //declare object types
 const BookType = new  GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         title: {type: GraphQLString},
-        category: {type: GraphQLString}
+        category: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args){
+                console.log(parent);
+                return lodash.find(authors, {id: parent.authorId});
+            }
+        }
+    })
+});
+
+const AuthorType = new  GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return lodash.filter(books, {authorId: parent.id});
+            }
+        }
     })
 });
 
@@ -26,9 +64,17 @@ const RootQuery = new  GraphQLObjectType({
         // get book by ID
         book: {
             type: BookType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent,args){
                 return lodash.find(books, {id: args.id});
+            }
+        },
+        // get author by ID
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent,args){
+                return lodash.find(authors, {id: args.id});
             }
         }
     }
